200e6a522d8caf52eefaf06f19c47e6be83acae7
suzanne <suzanne.aitchison@gmail.com>
2017-11-26 21:15:54 +0000

    adds basic exoplayer view

--- app/src/main/java/com/example/suzanne/recipesapp/StepDetailFragment.java
+++ app/src/main/java/com/example/suzanne/recipesapp/StepDetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.suzanne.recipesapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.suzanne.recipesapp.models.RecipeStep;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,9 +36,13 @@ public class StepDetailFragment extends Fragment {
     private static final String RECIPE_STEP_DETAIL_KEY = "recipeStepDetail";
 
     private RecipeStep mRecipeStep;
+    private ExoPlayer mExoPlayer;
 
     @BindView(R.id.tv_step_description)
     TextView mStepDescription;
+
+    @BindView(R.id.player_view)
+    SimpleExoPlayerView mPlayerView;
 
     @Nullable
     @Override
@@ -36,8 +54,41 @@ public class StepDetailFragment extends Fragment {
         if (bundle != null){
             mRecipeStep = bundle.getParcelable(RECIPE_STEP_DETAIL_KEY);
             mStepDescription.setText(mRecipeStep.getDescription());
+
+            initializePlayer();
         }
 
         return rootView;
+    }
+
+
+    private void initializePlayer(){
+        if (mExoPlayer == null){
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            mPlayerView.setPlayer((SimpleExoPlayer) mExoPlayer);
+
+            String userAgent = Util.getUserAgent(getActivity(), "recipe app");
+            Uri uri = Uri.parse(mRecipeStep.getVideoUrl());
+            MediaSource mediaSource = new ExtractorMediaSource(uri, new DefaultDataSourceFactory(
+                    getActivity(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+
+    private void releasePlayer(){
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        releasePlayer();
     }
 }
