bb178a130bc92064c33e85c0c0857664633eac92
suzanne <suzanne.aitchison@gmail.com>
2017-12-11 11:52:10 +0000

    hide arrows on widget if forward or back not available

--- app/src/main/java/com/example/suzanne/recipesapp/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/suzanne/recipesapp/RecipeWidgetProvider.java
@@ -7,7 +7,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.opengl.Visibility;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
@@ -25,25 +27,36 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     private static final String RECIPES_FULL_LIST_KEY = "recipesFullList";
     private static final String CURRENT_WIDGET_RECIPE_KEY = "currentRecipe";
     private static final String ON_NEXT_CLICK_TAG = "nextClick";
+    private static final String ON_BACK_CLICK_TAG = "backClick";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
         Intent listViewIntent = new Intent(context, ListViewWidgetService.class);
         views.setRemoteAdapter(R.id.lv_widget_ingredients, listViewIntent);
         views.setOnClickPendingIntent(R.id.widget_next_arrow,
                 getPendingSelfIntent(context, ON_NEXT_CLICK_TAG));
+        views.setOnClickPendingIntent(R.id.widget_back_button,
+                getPendingSelfIntent(context, ON_BACK_CLICK_TAG));
 
 
-//        Get the latest recipes list from shared prefs, and set the content to the first recipe
-        SharedPreferences preferences = context.getSharedPreferences(RECIPES_SHARED_PREF, Context.MODE_PRIVATE);
-        String recipesJson = preferences.getString(RECIPES_FULL_LIST_KEY, "");
-        Gson gson = new Gson();
-        Recipe[] recipes = gson.fromJson(recipesJson, Recipe[].class);
-        int currentRecipeIndex = preferences.getInt(CURRENT_WIDGET_RECIPE_KEY, 0);
-        Log.d("current recipe index", String.valueOf(currentRecipeIndex));
+//        Get the latest recipes list from shared prefs, and set the title accordingly
+        Recipe[] recipes = getSharedPreferencesRecipes(context);
+        int currentRecipeIndex = getCurrentRecipeIndex(context);
+
+//        Hide arrows if no back/forward option
+        if (recipes.length - 1 == currentRecipeIndex){
+            views.setViewVisibility(R.id.widget_next_arrow, View.INVISIBLE);
+        } else {
+            views.setViewVisibility(R.id.widget_next_arrow, View.VISIBLE);
+        }
+
+        if (currentRecipeIndex == 0){
+            views.setViewVisibility(R.id.widget_back_button, View.INVISIBLE);
+        } else {
+            views.setViewVisibility(R.id.widget_back_button, View.VISIBLE);
+        }
 
         views.setTextViewText(R.id.tv_widget_recipe_title, recipes[currentRecipeIndex].getName());
 //        Update the listview
@@ -86,35 +99,37 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent){
         super.onReceive(context, intent);
+        SharedPreferences preferences = context.getSharedPreferences(RECIPES_SHARED_PREF, Context.MODE_PRIVATE);
+        int currentIndex = preferences.getInt(CURRENT_WIDGET_RECIPE_KEY, 0);
+
+        SharedPreferences.Editor editor = preferences.edit();
+
         if(ON_NEXT_CLICK_TAG.equals(intent.getAction())){
-            Log.d("recieved click", "click");
-            SharedPreferences preferences = context.getSharedPreferences(RECIPES_SHARED_PREF, Context.MODE_PRIVATE);
-            int currentIndex = preferences.getInt(CURRENT_WIDGET_RECIPE_KEY, 0);
-            SharedPreferences.Editor editor = preferences.edit();
             editor.putInt(CURRENT_WIDGET_RECIPE_KEY, (currentIndex + 1));
             editor.commit();
 
-
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), RecipeWidgetProvider.class.getName());
-            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
-            onUpdate(context, appWidgetManager, appWidgetIds);
-
-//            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, RecipeWidgetProvider.class));
-//            onUpdate(context, appWidgetManager, appWidgetIds);
-//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-//            appWidgetManager.updateAppWidget(appWidgetIds, views);
-//            Intent updateIntent = new Intent(context, RecipeWidgetProvider.class);
-//            updateIntent.setAction(appWidgetManager.ACTION_APPWIDGET_UPDATE);
-//
-//            updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-//            context.sendBroadcast(updateIntent);
-
-
-
-
-
+        } else if (ON_BACK_CLICK_TAG.equals(intent.getAction())){
+            editor.putInt(CURRENT_WIDGET_RECIPE_KEY, (currentIndex - 1));
+            editor.commit();
         }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName thisAppWidget = new ComponentName(context.getPackageName(), RecipeWidgetProvider.class.getName());
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+        onUpdate(context, appWidgetManager, appWidgetIds);
     }
+
+    private static Recipe[] getSharedPreferencesRecipes(Context context){
+        SharedPreferences preferences = context.getSharedPreferences(RECIPES_SHARED_PREF, Context.MODE_PRIVATE);
+        String recipesJson = preferences.getString(RECIPES_FULL_LIST_KEY, "");
+        Gson gson = new Gson();
+        return gson.fromJson(recipesJson, Recipe[].class);
+    }
+
+    private static int getCurrentRecipeIndex(Context context){
+        SharedPreferences preferences = context.getSharedPreferences(RECIPES_SHARED_PREF, Context.MODE_PRIVATE);
+        return preferences.getInt(CURRENT_WIDGET_RECIPE_KEY, 0);
+    }
+
 }
 
