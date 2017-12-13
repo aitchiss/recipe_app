f919787538dc5b43a3e90348c8bf75534544a6ca
suzanne <suzanne.aitchison@gmail.com>
2017-12-13 07:53:29 +0000

    restore scroll state on master recipe detail fragment

--- app/src/main/java/com/example/suzanne/recipesapp/RecipeActivity.java
+++ app/src/main/java/com/example/suzanne/recipesapp/RecipeActivity.java
@@ -1,6 +1,7 @@
 package com.example.suzanne.recipesapp;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,7 @@ public class RecipeActivity extends AppCompatActivity implements MasterRecipeDet
     private static final String IS_FIRST_STEP_KEY = "isFirstStep";
     private static final String FLAG_NEXT = "next";
     private static final String FLAG_PREVIOUS = "previous";
+    private static final String STATE_RECIPE_KEY = "recipeState";
     private Recipe mRecipe;
     private int mCurrentStepIndex;
     private boolean mTwoPaneMode;
@@ -31,35 +33,40 @@ public class RecipeActivity extends AppCompatActivity implements MasterRecipeDet
         Bundle bundle = new Bundle();
         bundle.putParcelable(RECIPE_ACTIVITY_EXTRA_KEY, mRecipe);
 
-        MasterRecipeDetailFragment recipeDetailFragment = new MasterRecipeDetailFragment();
-        recipeDetailFragment.setArguments(bundle);
+        if(savedInstanceState == null){
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.master_recipe_detail_container, recipeDetailFragment)
-                .commit();
+            MasterRecipeDetailFragment recipeDetailFragment = new MasterRecipeDetailFragment();
+            recipeDetailFragment.setArguments(bundle);
 
-        if(findViewById(R.id.recipe_horiztonal_layout) != null){
-            mTwoPaneMode = true;
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.master_recipe_detail_container, recipeDetailFragment)
+                    .commit();
 
-            bundle.putParcelable(RECIPE_STEP_DETAIL_KEY, mRecipe.getSteps()[mCurrentStepIndex]);
-            if(mRecipe.getSteps().length - 1 == mCurrentStepIndex){
-                bundle.putBoolean(IS_LAST_STEP_KEY, true);
+            if(findViewById(R.id.recipe_horiztonal_layout) != null){
+                mTwoPaneMode = true;
+
+                bundle.putParcelable(RECIPE_STEP_DETAIL_KEY, mRecipe.getSteps()[mCurrentStepIndex]);
+                if(mRecipe.getSteps().length - 1 == mCurrentStepIndex){
+                    bundle.putBoolean(IS_LAST_STEP_KEY, true);
+                } else {
+                    bundle.putBoolean(IS_LAST_STEP_KEY, false);
+                }
+                bundle.putBoolean(IS_FIRST_STEP_KEY, (mCurrentStepIndex == 0));
+                if(savedInstanceState == null){
+                    StepDetailFragment stepDetailFragment = new StepDetailFragment();
+                    stepDetailFragment.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.step_detail_fragment_container, stepDetailFragment)
+                            .commit();
+                }
             } else {
-                bundle.putBoolean(IS_LAST_STEP_KEY, false);
+                mTwoPaneMode = false;
             }
-            bundle.putBoolean(IS_FIRST_STEP_KEY, (mCurrentStepIndex == 0));
-            if(savedInstanceState == null){
-                StepDetailFragment stepDetailFragment = new StepDetailFragment();
-                stepDetailFragment.setArguments(bundle);
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.step_detail_fragment_container, stepDetailFragment)
-                        .commit();
-            }
-        } else {
-            mTwoPaneMode = false;
+
+            getSupportActionBar().setTitle(mRecipe.getName());
         }
 
-        getSupportActionBar().setTitle(mRecipe.getName());
+
     }
 
     @Override
@@ -108,5 +115,19 @@ public class RecipeActivity extends AppCompatActivity implements MasterRecipeDet
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.step_detail_fragment_container, stepDetailFragment)
                 .commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putParcelable(STATE_RECIPE_KEY, mRecipe);
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null && savedInstanceState.containsKey(STATE_RECIPE_KEY)){
+            mRecipe = savedInstanceState.getParcelable(STATE_RECIPE_KEY);
+        }
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
